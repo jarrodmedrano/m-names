f3eeae9549bbebf0031558cfb8fff9bba9b91202
Jarrod Medrano <jmedran@gmail.com>
2019-12-15 20:43:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {names} from './names.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ul>
+        { 
+          names.filter((item, index) => {
+              return names.indexOf(item) === index
+          }).map((name, id) => {
+            return (
+                <React.Fragment>
+                  <li key={id} >
+                    {name.name}
+                  </li>
+                </React.Fragment>
+            )
+          })
+        }
+        </ul>
     </div>
   );
 }
---
+++ src/names.js
@@ -0,0 +1,1586 @@
+export const names = [
+{
+  "name": "Maduro"
+},
+{
+  "name": "Maes"
+},
+{
+  "name": "Maese"
+},
+{
+  "name": "Maez"
+},
+{
+  "name": "Magallanes"
+},
+{
+  "name": "Magallanez"
+},
+{
+  "name": "Magallon"
+},
+{
+  "name": "Magan"
+},
+{
+  "name": "Magana"
+},
+{
+  "name": "Magdaleno"
+},
+{
+  "name": "Magin"
+},
+{
+  "name": "Majano"
+},
+{
+  "name": "Malagon"
+},
+{
+  "name": "Malara"
+},
+{
+  "name": "Malave"
+},
+{
+  "name": "Maldonado"
+},
+{
+  "name": "Maldonado"
+},
+{
+  "name": "Mallen"
+},
+{
+  "name": "Malo"
+},
+{
+  "name": "Malon"
+},
+{
+  "name": "Mamon"
+},
+{
+  "name": "Manas"
+},
+{
+  "name": "Mancebo"
+},
+{
+  "name": "Mancha"
+},
+{
+  "name": "Mancha"
+},
+{
+  "name": "Mancilla"
+},
+{
+  "name": "Mancillas"
+},
+{
+  "name": "Mandes"
+},
+{
+  "name": "Mandujano"
+},
+{
+  "name": "Mangas"
+},
+{
+  "name": "Mangas"
+},
+{
+  "name": "Manjarrez"
+},
+{
+  "name": "Mano"
+},
+{
+  "name": "Manos"
+},
+{
+  "name": "Manrique"
+},
+{
+  "name": "Manriquez"
+},
+{
+  "name": "Mansilla"
+},
+{
+  "name": "Manso"
+},
+{
+  "name": "Mantilla"
+},
+{
+  "name": "Manuel"
+},
+{
+  "name": "Manzanares"
+},
+{
+  "name": "Manzano"
+},
+{
+  "name": "Mar"
+},
+{
+  "name": "Maradiaga"
+},
+{
+  "name": "Maravilla"
+},
+{
+  "name": "Marcano"
+},
+{
+  "name": "Marcial"
+},
+{
+  "name": "Marciel"
+},
+{
+  "name": "Marco"
+},
+{
+  "name": "Marcos"
+},
+{
+  "name": "Mabeyra"
+},
+{
+  "name": "Macarro"
+},
+{
+  "name": "Macaya"
+},
+{
+  "name": "Maceira"
+},
+{
+  "name": "Machado"
+},
+{
+  "name": "Machín"
+},
+{
+  "name": "Macho"
+},
+{
+  "name": "Machuca"
+},
+{
+  "name": "Macià"
+},
+{
+  "name": "Macián"
+},
+{
+  "name": "Macías"
+},
+{
+  "name": "Madariaga"
+},
+{
+  "name": "Madejón"
+},
+{
+  "name": "Madera"
+},
+{
+  "name": "Madero"
+},
+{
+  "name": "Madí"
+},
+{
+  "name": "Madina"
+},
+{
+  "name": "Madinaveitia"
+},
+{
+  "name": "Madrazo"
+},
+{
+  "name": "Madrid"
+},
+{
+  "name": "Madrigal"
+},
+{
+  "name": "Madrilejos"
+},
+{
+  "name": "Madriñan"
+},
+{
+  "name": "Madurga"
+},
+{
+  "name": "Maduro"
+},
+{
+  "name": "Maestre"
+},
+{
+  "name": "Maestro"
+},
+{
+  "name": "Mafages"
+},
+{
+  "name": "Magallón"
+},
+{
+  "name": "Magán"
+},
+{
+  "name": "Magaña"
+},
+{
+  "name": "Magarzo"
+},
+{
+  "name": "Magdalena"
+},
+{
+  "name": "Magrazo"
+},
+{
+  "name": "Maicas"
+},
+{
+  "name": "Maillo"
+},
+{
+  "name": "Mainé"
+},
+{
+  "name": "Maire"
+},
+{
+  "name": "Majado"
+},
+{
+  "name": "Maldonado"
+},
+{
+  "name": "Malet"
+},
+{
+  "name": "Malgor"
+},
+{
+  "name": "Malik"
+},
+{
+  "name": "Mallabia"
+},
+{
+  "name": "Mallafré"
+},
+{
+  "name": "Mallagaray"
+},
+{
+  "name": "Mallorquí"
+},
+{
+  "name": "Malmierca"
+},
+{
+  "name": "Malo"
+},
+{
+  "name": "Maluenda"
+},
+{
+  "name": "Maluqué"
+},
+{
+  "name": "Malvar"
+},
+{
+  "name": "Mambrilla"
+},
+{
+  "name": "Mamolar"
+},
+{
+  "name": "Mañá"
+},
+{
+  "name": "Mañach"
+},
+{
+  "name": "Mañarandegui"
+},
+{
+  "name": "Mañas"
+},
+{
+  "name": "Mancada"
+},
+{
+  "name": "Mancebo"
+},
+{
+  "name": "Mancera"
+},
+{
+  "name": "Manchón"
+},
+{
+  "name": "Mañé"
+},
+{
+  "name": "Maneiro"
+},
+{
+  "name": "Manent"
+},
+{
+  "name": "Manero"
+},
+{
+  "name": "Mañes"
+},
+{
+  "name": "Manjón"
+},
+{
+  "name": "Mannaut"
+},
+{
+  "name": "Manotas"
+},
+{
+  "name": "Manrique"
+},
+{
+  "name": "Mansilla"
+},
+{
+  "name": "Manso"
+},
+{
+  "name": "Manterola"
+},
+{
+  "name": "Manzana"
+},
+{
+  "name": "Manzanares"
+},
+{
+  "name": "Manzaneda"
+},
+{
+  "name": "Manzanera"
+},
+{
+  "name": "Manzano"
+},
+{
+  "name": "Manzanos"
+},
+{
+  "name": "Manzera"
+},
+{
+  "name": "Manzi"
+},
+{
+  "name": "Maqueda"
+},
+{
+  "name": "Maquibar"
+},
+{
+  "name": "Maquieira"
+},
+{
+  "name": "Maquiera"
+},
+{
+  "name": "Marañas"
+},
+{
+  "name": "Marañés"
+},
+{
+  "name": "Marañón"
+},
+{
+  "name": "Marcé"
+},
+{
+  "name": "Marcet"
+},
+{
+  "name": "March"
+},
+{
+  "name": "Marchante"
+},
+{
+  "name": "Marcilla"
+},
+{
+  "name": "Marco"
+},
+{
+  "name": "Marcos"
+},
+{
+  "name": "Mareca"
+},
+{
+  "name": "Marentes"
+},
+{
+  "name": "Mareque"
+},
+{
+  "name": "Marganell"
+},
+{
+  "name": "Margarida"
+},
+{
+  "name": "Margarit"
+},
+{
+  "name": "Margens"
+},
+{
+  "name": "Marí"
+},
+{
+  "name": "Marías"
+},
+{
+  "name": "Mariezkurrena"
+},
+{
+  "name": "Marín"
+},
+{
+  "name": "Marina"
+},
+{
+  "name": "Mariño"
+},
+{
+  "name": "Mariscal"
+},
+{
+  "name": "Marjés"
+},
+{
+  "name": "Marleza"
+},
+{
+  "name": "Marmaneu"
+},
+{
+  "name": "Maroto"
+},
+{
+  "name": "Marqués"
+},
+{
+  "name": "Marqueta"
+},
+{
+  "name": "Márquez"
+},
+{
+  "name": "Marquillo"
+},
+{
+  "name": "Marquina"
+},
+{
+  "name": "Marrón"
+},
+{
+  "name": "Marsá"
+},
+{
+  "name": "Marsans"
+},
+{
+  "name": "Martí"
+},
+{
+  "name": "Martín"
+},
+{
+  "name": "Martín-Guerrero"
+},
+{
+  "name": "Martín-Luengo"
+},
+{
+  "name": "Martín-Maestro"
+},
+{
+  "name": "Martín-Portugués"
+},
+{
+  "name": "Martinell"
+},
+{
+  "name": "Martínez"
+},
+{
+  "name": "Martínez-Caja"
+},
+{
+  "name": "Martínez-Mamblona"
+},
+{
+  "name": "Martínez-Mendiola"
+},
+{
+  "name": "Martínez-Moreno"
+},
+{
+  "name": "Martínez-Santa"
+},
+{
+  "name": "Martínez-Toledano"
+},
+{
+  "name": "Martins"
+},
+{
+  "name": "Martitegui"
+},
+{
+  "name": "Martorell"
+},
+{
+  "name": "Martos"
+},
+{
+  "name": "Martras"
+},
+{
+  "name": "Marty"
+},
+{
+  "name": "Marugán"
+},
+{
+  "name": "Maruri"
+},
+{
+  "name": "Marzal"
+},
+{
+  "name": "Marzo"
+},
+{
+  "name": "Mas"
+},
+{
+  "name": "Masalies"
+},
+{
+  "name": "Masana"
+},
+{
+  "name": "Mascarell"
+},
+{
+  "name": "Mascareñas"
+},
+{
+  "name": "Mascaros"
+},
+{
+  "name": "Mascort"
+},
+{
+  "name": "Masdeu"
+},
+{
+  "name": "Maseda"
+},
+{
+  "name": "Masegosa"
+},
+{
+  "name": "Masegué"
+},
+{
+  "name": "Masfenoll"
+},
+{
+  "name": "Masferrer"
+},
+{
+  "name": "Masià"
+},
+{
+  "name": "Masifern"
+},
+{
+  "name": "Masip"
+},
+{
+  "name": "Masnou"
+},
+{
+  "name": "Masó"
+},
+{
+  "name": "Massa"
+},
+{
+  "name": "Massagué"
+},
+{
+  "name": "Massaguer"
+},
+{
+  "name": "Massana"
+},
+{
+  "name": "Massanet"
+},
+{
+  "name": "Massó"
+},
+{
+  "name": "Massuet"
+},
+{
+  "name": "Mata"
+},
+{
+  "name": "Matalobos"
+},
+{
+  "name": "Matarredona"
+},
+{
+  "name": "Matas"
+},
+{
+  "name": "Mate"
+},
+{
+  "name": "Mateo"
+},
+{
+  "name": "Mateos"
+},
+{
+  "name": "Mateu"
+},
+{
+  "name": "Matheo"
+},
+{
+  "name": "Matheos"
+},
+{
+  "name": "Matheu"
+},
+{
+  "name": "Mathías"
+},
+{
+  "name": "Mathías-Fernández"
+},
+{
+  "name": "Matías"
+},
+{
+  "name": "Matos"
+},
+{
+  "name": "Matute"
+},
+{
+  "name": "Mauleón"
+},
+{
+  "name": "Maura"
+},
+{
+  "name": "Mauri"
+},
+{
+  "name": "Maya"
+},
+{
+  "name": "Mayayo"
+},
+{
+  "name": "Maycas"
+},
+{
+  "name": "Mayllo"
+},
+{
+  "name": "Maymó"
+},
+{
+  "name": "Maynar"
+},
+{
+  "name": "Mayo"
+},
+{
+  "name": "Mayol"
+},
+{
+  "name": "Mayor"
+},
+{
+  "name": "Mayora"
+},
+{
+  "name": "Mayoral"
+},
+{
+  "name": "Mayorena"
+},
+{
+  "name": "Maza"
+},
+{
+  "name": "Mazo"
+},
+{
+  "name": "Meca"
+},
+{
+  "name": "Mechó"
+},
+{
+  "name": "Medel"
+},
+{
+  "name": "Medialdea"
+},
+{
+  "name": "Mediano"
+},
+{
+  "name": "Mediavilla"
+},
+{
+  "name": "Medina"
+},
+{
+  "name": "Megías"
+},
+{
+  "name": "Mejias"
+},
+{
+  "name": "Melado"
+},
+{
+  "name": "Melero"
+},
+{
+  "name": "Melgarejo"
+},
+{
+  "name": "Melich"
+},
+{
+  "name": "Mellado"
+},
+{
+  "name": "Melo"
+},
+{
+  "name": "Melús"
+},
+{
+  "name": "Mena"
+},
+{
+  "name": "Menacho"
+},
+{
+  "name": "Menargues"
+},
+{
+  "name": "Menchón"
+},
+{
+  "name": "Mencía"
+},
+{
+  "name": "Mendasona"
+},
+{
+  "name": "Méndez"
+},
+{
+  "name": "Méndez-Antequera"
+},
+{
+  "name": "Mendia"
+},
+{
+  "name": "Mendico"
+},
+{
+  "name": "Mendieta"
+},
+{
+  "name": "Mendiola"
+},
+{
+  "name": "Mendiondo"
+},
+{
+  "name": "Mendivil"
+},
+{
+  "name": "Mendizabal"
+},
+{
+  "name": "Mendoza"
+},
+{
+  "name": "Mendraca"
+},
+{
+  "name": "Menea"
+},
+{
+  "name": "Menéndez"
+},
+{
+  "name": "Menoyo"
+},
+{
+  "name": "Mer"
+},
+{
+  "name": "Mera"
+},
+{
+  "name": "Mercader"
+},
+{
+  "name": "Merchán"
+},
+{
+  "name": "Merchante"
+},
+{
+  "name": "Merica"
+},
+{
+  "name": "Mericaechevarría"
+},
+{
+  "name": "Merino"
+},
+{
+  "name": "Merlo"
+},
+{
+  "name": "Meroño"
+},
+{
+  "name": "Merquelanz"
+},
+{
+  "name": "Meruelo"
+},
+{
+  "name": "Mesa"
+},
+{
+  "name": "Mesas"
+},
+{
+  "name": "Mesclans"
+},
+{
+  "name": "Mesonero"
+},
+{
+  "name": "Mestras"
+},
+{
+  "name": "Mestres"
+},
+{
+  "name": "Mexías"
+},
+{
+  "name": "Mezodio"
+},
+{
+  "name": "Mezquita"
+},
+{
+  "name": "Miangolarra"
+},
+{
+  "name": "Micás"
+},
+{
+  "name": "Michalak"
+},
+{
+  "name": "Michelena"
+},
+{
+  "name": "MIchelendiaga"
+},
+{
+  "name": "Miedes"
+},
+{
+  "name": "Mielgo"
+},
+{
+  "name": "Miera"
+},
+{
+  "name": "Miguel"
+},
+{
+  "name": "Miguelartcaina"
+},
+{
+  "name": "Míguez"
+},
+{
+  "name": "Mijares"
+},
+{
+  "name": "Milà"
+},
+{
+  "name": "Milamor"
+},
+{
+  "name": "Milego"
+},
+{
+  "name": "Milesi"
+},
+{
+  "name": "Milia"
+},
+{
+  "name": "Milla"
+},
+{
+  "name": "Millán"
+},
+{
+  "name": "Millat"
+},
+{
+  "name": "Millinedo"
+},
+{
+  "name": "Miñambres"
+},
+{
+  "name": "Miñana"
+},
+{
+  "name": "Miñarro"
+},
+{
+  "name": "Minen"
+},
+{
+  "name": "Mingo"
+},
+{
+  "name": "Mingolarra"
+},
+{
+  "name": "Mingot"
+},
+{
+  "name": "Mingote"
+},
+{
+  "name": "Minguell"
+},
+{
+  "name": "Minguet"
+},
+{
+  "name": "Mínguez"
+},
+{
+  "name": "Miniño"
+},
+{
+  "name": "Ministral"
+},
+{
+  "name": "Minobis"
+},
+{
+  "name": "Mintegui"
+},
+{
+  "name": "Minteguía"
+},
+{
+  "name": "Miquel"
+},
+{
+  "name": "Mir"
+},
+{
+  "name": "Mirallas"
+},
+{
+  "name": "Miralles"
+},
+{
+  "name": "Miralsol"
+},
+{
+  "name": "Mirambell"
+},
+{
+  "name": "Miranda"
+},
+{
+  "name": "Miras"
+},
+{
+  "name": "Mirmi"
+},
+{
+  "name": "Miró"
+},
+{
+  "name": "Mirón"
+},
+{
+  "name": "Misa"
+},
+{
+  "name": "Mitjans"
+},
+{
+  "name": "Mogollón"
+},
+{
+  "name": "Mohedano"
+},
+{
+  "name": "Moixí"
+},
+{
+  "name": "Moja"
+},
+{
+  "name": "Moldes"
+},
+{
+  "name": "Molero"
+},
+{
+  "name": "Molina"
+},
+{
+  "name": "Molinares"
+},
+{
+  "name": "Moliner"
+},
+{
+  "name": "Molinero"
+},
+{
+  "name": "Molino"
+},
+{
+  "name": "Molins"
+},
+{
+  "name": "Molist"
+},
+{
+  "name": "Moll"
+},
+{
+  "name": "Mollinedo"
+},
+{
+  "name": "Mombiella"
+},
+{
+  "name": "Monasterio"
+},
+{
+  "name": "Moncalbillo"
+},
+{
+  "name": "Moncalvillo"
+},
+{
+  "name": "Moncau"
+},
+{
+  "name": "Moncín"
+},
+{
+  "name": "Monclús"
+},
+{
+  "name": "Moncosí"
+},
+{
+  "name": "Moncunill"
+},
+{
+  "name": "Mondéjar"
+},
+{
+  "name": "Monedero"
+},
+{
+  "name": "Monferrer"
+},
+{
+  "name": "Monge"
+},
+{
+  "name": "Moniche"
+},
+{
+  "name": "Monin"
+},
+{
+  "name": "Monje"
+},
+{
+  "name": "Monlleó"
+},
+{
+  "name": "Monrabal"
+},
+{
+  "name": "Monràs"
+},
+{
+  "name": "Monreal"
+},
+{
+  "name": "Monroig"
+},
+{
+  "name": "Mons"
+},
+{
+  "name": "Monsálvez"
+},
+{
+  "name": "Monseo"
+},
+{
+  "name": "Mont"
+},
+{
+  "name": "Montada"
+},
+{
+  "name": "Montalbán"
+},
+{
+  "name": "Montalt"
+},
+{
+  "name": "Montalvo"
+},
+{
+  "name": "Montaña"
+},
+{
+  "name": "Montaner"
+},
+{
+  "name": "Montañez"
+},
+{
+  "name": "Montaño"
+},
+{
+  "name": "Montany"
+},
+{
+  "name": "Montanya"
+},
+{
+  "name": "Montariol"
+},
+{
+  "name": "Montasell"
+},
+{
+  "name": "Montava"
+},
+{
+  "name": "Monte"
+},
+{
+  "name": "Monteagudo"
+},
+{
+  "name": "Monteira"
+},
+{
+  "name": "Monteiro"
+},
+{
+  "name": "Montejano"
+},
+{
+  "name": "Montejo"
+},
+{
+  "name": "Montenegro"
+},
+{
+  "name": "Montero"
+},
+{
+  "name": "Montes"
+},
+{
+  "name": "Montesa"
+},
+{
+  "name": "Montesdeoca"
+},
+{
+  "name": "Monteserin"
+},
+{
+  "name": "Montesinos"
+},
+{
+  "name": "Monteso"
+},
+{
+  "name": "Montiel"
+},
+{
+  "name": "Montilla"
+},
+{
+  "name": "Montori"
+},
+{
+  "name": "Montoro"
+},
+{
+  "name": "Montosa"
+},
+{
+  "name": "Montoya"
+},
+{
+  "name": "Montras"
+},
+{
+  "name": "Montserrat"
+},
+{
+  "name": "Monzó"
+},
+{
+  "name": "Mor"
+},
+{
+  "name": "Mora"
+},
+{
+  "name": "Moradell"
+},
+{
+  "name": "Moraga"
+},
+{
+  "name": "Moragón"
+},
+{
+  "name": "Moral"
+},
+{
+  "name": "Morales"
+},
+{
+  "name": "Morán"
+},
+{
+  "name": "Morante"
+},
+{
+  "name": "Moras"
+},
+{
+  "name": "Moratalla"
+},
+{
+  "name": "Moratilla"
+},
+{
+  "name": "Moratín"
+},
+{
+  "name": "Moratinos"
+},
+{
+  "name": "Morcillo"
+},
+{
+  "name": "Morcuendez"
+},
+{
+  "name": "Moré"
+},
+{
+  "name": "Moreda"
+},
+{
+  "name": "Moreira"
+},
+{
+  "name": "Morejón"
+},
+{
+  "name": "Morel"
+},
+{
+  "name": "Morell"
+},
+{
+  "name": "Morena"
+},
+{
+  "name": "Moreno"
+},
+{
+  "name": "Moreno-Castellanos"
+},
+{
+  "name": "Moreno-Ortiz"
+},
+{
+  "name": "Morente"
+},
+{
+  "name": "Morentín"
+},
+{
+  "name": "Moreo"
+},
+{
+  "name": "Morera"
+},
+{
+  "name": "Morgado"
+},
+{
+  "name": "Morillas"
+},
+{
+  "name": "Morillo"
+},
+{
+  "name": "Morodo"
+},
+{
+  "name": "Morón"
+},
+{
+  "name": "Morote"
+},
+{
+  "name": "Morral"
+},
+{
+  "name": "Morras"
+},
+{
+  "name": "Morro"
+},
+{
+  "name": "Morte"
+},
+{
+  "name": "Mosteiro"
+},
+{
+  "name": "Mosteros"
+},
+{
+  "name": "Mota"
+},
+{
+  "name": "Moto"
+},
+{
+  "name": "Moure"
+},
+{
+  "name": "Mourin"
+},
+{
+  "name": "Mouriño"
+},
+{
+  "name": "Mouriz"
+},
+{
+  "name": "Moya"
+},
+{
+  "name": "Moyano"
+},
+{
+  "name": "Muerza"
+},
+{
+  "name": "Múgica"
+},
+{
+  "name": "Muguerza"
+},
+{
+  "name": "Muguruza"
+},
+{
+  "name": "Muixó"
+},
+{
+  "name": "Mula"
+},
+{
+  "name": "Mulero"
+},
+{
+  "name": "Mulet"
+},
+{
+  "name": "Muñagorri"
+},
+{
+  "name": "Munain"
+},
+{
+  "name": "Munar"
+},
+{
+  "name": "Munarriz"
+},
+{
+  "name": "Mundet"
+},
+{
+  "name": "Mundí"
+},
+{
+  "name": "Muniategui"
+},
+{
+  "name": "Muniesa"
+},
+{
+  "name": "Munilla"
+},
+{
+  "name": "Munin"
+},
+{
+  "name": "Muniosguren"
+},
+{
+  "name": "Muñiz"
+},
+{
+  "name": "Muñoa"
+},
+{
+  "name": "Muñoz"
+},
+{
+  "name": "Muns"
+},
+{
+  "name": "Muñumer"
+},
+{
+  "name": "Munyesch"
+},
+{
+  "name": "Mur"
+},
+{
+  "name": "Murcia"
+},
+{
+  "name": "Murga"
+},
+{
+  "name": "Murillo"
+},
+{
+  "name": "Murillo-Benítez"
+},
+{
+  "name": "Muro"
+},
+{
+  "name": "Murojosa"
+},
+{
+  "name": "Muru"
+},
+{
+  "name": "Murua"
+},
+{
+  "name": "Muruaga"
+},
+{
+  "name": "Murugarren"
+},
+{
+  "name": "Musachs"
+},
+{
+  "name": "Musarro"
+},
+{
+  "name": "Muslares"
+},
+{
+  "name": "Múxica"
+},
+{
+  "name": "Muzás"
+}
+]
